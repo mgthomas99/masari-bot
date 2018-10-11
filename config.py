de99d7f22a6a4cb7ee4477dac51bdfe76181072a
mgthomas99 <m.g.thomas99@gmail.com>
2018-10-11 23:34:31 +0100

    config categories

--- config.py
+++ config.py
@@ -1,15 +1,8 @@
 import configparser
 
 config = configparser.ConfigParser()
+config.read("config.ini")
 
-config.read('config.ini')
-
-discordtoken = config['SETTINGS']['discordtoken'].strip()
-server = config['SETTINGS']['server'].strip()
-welcomeChannel = config['SETTINGS']['welcomeChannel'].strip()
-exchangeChannel = config['SETTINGS']['exchangeChannel'].strip()
-marketChannel = config['SETTINGS']['marketChannel'].strip()
-networkChannel = config['SETTINGS']['networkChannel'].strip()
-apiURL = config['SETTINGS']['apiURL'].strip()
-blockTargetTime = config['SETTINGS']['blockTargetTime'].strip()
-coinCode = config['SETTINGS']['coinCode'].strip()
+discord = config["Discord"]
+service = config["Service"]
+masari = config["Masari"]
